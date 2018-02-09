f5b9aa864e17fed78fa078cc46d5771b60f8137f
Brent  Ransom <sadumedusa@gmail.com>
2018-02-09 02:57:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,12 +7,14 @@ class Artist
   end
 
   def add_song(song)
-    @song << song
+    @songs << song
     song.artist = self
   end
 
   def add_song_by_name(song, artist)
     song = Song.new(song, artist)
+    @songs << song
+    song.artist = self
   end
 
   def songs
